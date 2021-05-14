6c714661e291af4b7a309602537b3af919440bd7
junidevs <52135894+junidevs@users.noreply.github.com>
2021-05-14 23:34:25 +0200

    Update FreeDay.js

--- client/src/components/tasks/FreeDay.js
+++ client/src/components/tasks/FreeDay.js
@@ -1,18 +1,36 @@
 import React from 'react'
-import {Link } from 'react-router-dom';
-import Modal from '../Modal';
-import history from '../../history';
-export default function FreeDay() {
+ import Modal from '../Modal';
+ import history from '../../history';
+ import {Link } from 'react-router-dom';
+
+const DeleteTask = ({date,setShowFreeDay})=> {
+    const dataString = JSON.stringify(date).substr(1,10);
 
     const actions = (
-        <>
-            <button onClick={()=>(match.params.id)} className="ui button positive">Yes</button>
-            <Link to='/' className="ui button">Cancel</Link>
-        </>
-   );
-    return (
-        <>
-            
-        </>
-    )
+        // what should be implemented: Check for free day against task. if freeday has been shceduled user shouldn't be able to add tasks
+         <>
+             <button 
+                 onClick={()=> {
+                localStorage.setItem('freeday',dataString);
+                return setShowFreeDay(false)
+                }} 
+                 className="ui button positive">Yes</button>
+                    <Link 
+                    onClick={()=> {return setShowFreeDay(false)}}
+                    to='/' 
+                    className="ui button">Cancel</Link>
+         </>
+    );
+
+    const renderContent = ()=>{
+        
+            return `Book free day  for ${dataString} ?`
+        
+       
+    }
+    return <Modal title={`${dataString}`} description={renderContent()} actions={actions} onDismiss={()=>history.push('/')} />
+    
 }
+
+
+export default (DeleteTask);
