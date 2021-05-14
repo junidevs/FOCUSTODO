1f646ca3c3234e9e18830e925016ef8980dbb002
junidevs <52135894+junidevs@users.noreply.github.com>
2021-05-14 23:34:56 +0200

    Update ListTasks.js

--- client/src/components/tasks/ListTasks.js
+++ client/src/components/tasks/ListTasks.js
@@ -5,15 +5,16 @@ import DatePicker from "react-datepicker";
 import {Link} from 'react-router-dom';
 import TaskRendererHelper from './TaskRendererHelper';
 import moment from "moment-timezone";
-
+import FreeDay from './FreeDay';
 const ListTasks = ({tasks,fetchTasks}) => {
 
-    const [date, setDate] = useState(()=>{
-        //small workaround for CET timezone
-        const UTC_timezone = new Date();
-       return UTC_timezone.setHours(UTC_timezone.getHours() + 2 );
+    const [showFreeDay,setShowFreeDay] = useState(false);
+    const [date, setDate] = useState(new Date()
+        //small workaround for CET timezone  
+       // const UTC_timezone = new Date();
+       //return UTC_timezone.setHours(UTC_timezone.getHours() + 2 );
         
-    });
+    );
     const handleCalendarClose = () => console.log("Calendar closed");
     const handleCalendarOpen = () => console.log("Calendar opened");
     const regexHandleDate = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
@@ -22,9 +23,19 @@ const ListTasks = ({tasks,fetchTasks}) => {
     useEffect(()=>{
         moment.tz.setDefault("Europe/Poland");
         fetchTasks();    
-    
+        console.log(showFreeDay);
     },[])
 
+    const handleFreeDay = ()=>{
+        
+           const freeday= localStorage.getItem('freeday');
+            
+           if(freeday)
+           {
+               return <h3 style={{margin:'15px 0px'}}>Next free day : {freeday}</h3>;
+           }
+           else return null;
+    }
     return (
         <div className="task_List_Container">
        
@@ -32,6 +43,7 @@ const ListTasks = ({tasks,fetchTasks}) => {
        <div className="__shortInfo">Tasks for selected date</div>
          
             <div className="__calendar_Box">
+                {handleFreeDay()}
                 <DatePicker
                     selected={date}
                     dateFormat='yyyy-MM-dd'
@@ -43,9 +55,10 @@ const ListTasks = ({tasks,fetchTasks}) => {
                      <Link className="ui button __create_newTask_button" to="todos/new">
               Create a new Task
             </Link>
+            <button style={{marginTop:'10px'}} className="ui button teal" onClick={()=> setShowFreeDay(!showFreeDay)}>Create free Day</button>
             </div>
           <div className="tasks_Container">
-       
+                {showFreeDay ? <FreeDay date={date} setShowFreeDay={setShowFreeDay} /> : null}
                 <TaskRendererHelper date={date} regexHandleDate={regexHandleDate} tasks={tasks}/>
             </div>
   
