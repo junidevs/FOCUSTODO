32576a5355457b914e242ca59bb38b7bbedea130
junidevs <52135894+junidevs@users.noreply.github.com>
2021-05-14 23:33:28 +0200

    Update CreateTask.js

--- client/src/components/tasks/CreateTask.js
+++ client/src/components/tasks/CreateTask.js
@@ -4,10 +4,8 @@ import {connect} from 'react-redux';
 import {createTask} from '../../actions/index';
 
 class CreateTask extends Component {
-  
-  
+    
     //formValues property contains all of values from our form FIELD components via name property
-    //also we should remeber redux-form react on each interaction , for example onChange value , when we leave input etc....
     onHandleSubmit = (formValues) =>{
         console.log(formValues);
         this.props.createTask(formValues);
@@ -24,5 +22,3 @@ class CreateTask extends Component {
 }
 
 export default connect(null,{createTask})(CreateTask);
-//co bedzie zawieral formularz
-// title,description,priority,day and month 
